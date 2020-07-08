c0fcee684bf96d2dda81ff7ec6f30cebb9fb6908
stars <wm76868@sina.com>
2020-07-08 15:33:41 +0800

    添加pom.xml中的依赖

--- security-user/src/main/java/com/security/user/controller/MiscellaneousController.java
+++ security-user/src/main/java/com/security/user/controller/MiscellaneousController.java
@@ -1,8 +1,12 @@
 package com.security.user.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
+ * 各种前端请求测试
  * @author: stars
  * @create: 2020年 07月 08日 10:47
  **/
+@RestController
 public class MiscellaneousController {
 }
